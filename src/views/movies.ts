398e4928e7496145415b37cc8247efd4e7a3f1cd
William Fonseca <santifon15@gmail.com>
2023-02-15 19:23:02 -0500

    create function to createMovieCard

--- src/views/movies.ts
+++ src/views/movies.ts
@@ -45,48 +45,7 @@ function createMovies(method: Promise<Movie[]>): void {
             movies.forEach(movie => {
                const divCard = document.createElement("div");
                divCard.className = "col-lg-3 col-md-4 col-12 p-2";
-               const divShadow = document.createElement("div");
-               divShadow.className = "card shadow-sm";
-               const img = document.createElement("img");
-               img.src = movie.poster_path ? "https://image.tmdb.org/t/p/w500/" + movie.poster_path : movie.original_title;
-               img.alt = movie.original_title;
-               const divFavIcon = document.createElement("div");
-               if (isInFavorites(movie.id)) {
-                  divFavIcon.innerHTML = `<svg id="${movie.id}" xmlns="http://www.w3.org/2000/svg" stroke="white" fill="#ff000078" width="50" height="50"
-                                class="bi bi-heart-fill position-absolute p-2" viewBox="0 -2 18 22">
-                                <path fill-rule="evenodd"
-                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                            </svg>`
-               } else {
-                  divFavIcon.innerHTML = `<svg id="${movie.id}" xmlns="http://www.w3.org/2000/svg" stroke="white" fill="#ff000078" width="50" height="50"
-                                class="bi bi-heart-fill position-absolute p-2" viewBox="0 -2 18 22">
-                                <path fill-rule="evenodd"
-                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" fill-opacity="0" fill-opacity="0"/>
-                            </svg>`
-               }
-               if (divFavIcon.firstChild) {
-                  divFavIcon.firstChild.addEventListener("click", () => {
-                     const op = addOrRemoveFavorite(movie.id);
-                     console.log(changeFavIcon(movie.id, op));
-                  });
-               }
-               const divBody = document.createElement("div");
-               divBody.className = "card-body";
-               const p = document.createElement("p");
-               p.className = "card-text truncate";
-               p.textContent = movie.overview;
-               const div = document.createElement("div");
-               div.className = "d-flex justify-content-between align-items-center";
-               const small = document.createElement("small");
-               small.className = "text-muted";
-               small.textContent = movie.release_date;
-               div.appendChild(small);
-               divBody.appendChild(p);
-               divBody.appendChild(div);
-               divShadow.appendChild(img);
-               divShadow.appendChild(<ChildNode>divFavIcon.firstChild);
-               divShadow.appendChild(divBody);
-               divCard.appendChild(divShadow);
+               createMovieCard(divCard, movie);
                container.appendChild(divCard);
             });
          })
@@ -96,6 +55,51 @@ function createMovies(method: Promise<Movie[]>): void {
    } else {
       alert("No container?");
    }
+}
+
+function createMovieCard(container: HTMLElement, movie: Movie): void {
+   const divShadow = document.createElement("div");
+   divShadow.className = "card shadow-sm";
+   const img = document.createElement("img");
+   img.src = movie.poster_path ? "https://image.tmdb.org/t/p/w500/" + movie.poster_path : movie.original_title;
+   img.alt = movie.original_title;
+   const divFavIcon = document.createElement("div");
+   if (isInFavorites(movie.id)) {
+      divFavIcon.innerHTML = `<svg id="${movie.id}" xmlns="http://www.w3.org/2000/svg" stroke="white" fill="#ff000078" width="50" height="50"
+                                class="bi bi-heart-fill position-absolute p-2" viewBox="0 -2 18 22">
+                                <path fill-rule="evenodd"
+                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                            </svg>`
+   } else {
+      divFavIcon.innerHTML = `<svg id="${movie.id}" xmlns="http://www.w3.org/2000/svg" stroke="white" fill="#ff000078" width="50" height="50"
+                                class="bi bi-heart-fill position-absolute p-2" viewBox="0 -2 18 22">
+                                <path fill-rule="evenodd"
+                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" fill-opacity="0" fill-opacity="0"/>
+                            </svg>`
+   }
+   if (divFavIcon.firstChild) {
+      divFavIcon.firstChild.addEventListener("click", () => {
+         const op = addOrRemoveFavorite(movie.id);
+         console.log(changeFavIcon(movie.id, op));
+      });
+   }
+   const divBody = document.createElement("div");
+   divBody.className = "card-body";
+   const p = document.createElement("p");
+   p.className = "card-text truncate";
+   p.textContent = movie.overview;
+   const div = document.createElement("div");
+   div.className = "d-flex justify-content-between align-items-center";
+   const small = document.createElement("small");
+   small.className = "text-muted";
+   small.textContent = movie.release_date;
+   div.appendChild(small);
+   divBody.appendChild(p);
+   divBody.appendChild(div);
+   divShadow.appendChild(img);
+   divShadow.appendChild(<ChildNode>divFavIcon.firstChild);
+   divShadow.appendChild(divBody);
+   container.appendChild(divShadow);
 }
 
 function addOrRemoveFavorite(id: number): string {
