3b4b592ac84a7680e07b5c18b916159ce2a40951
William Fonseca <santifon15@gmail.com>
2023-02-15 19:15:53 -0500

    remove comments

--- src/views/movies.ts
+++ src/views/movies.ts
@@ -50,15 +50,6 @@ function createMovies(method: Promise<Movie[]>): void {
                const img = document.createElement("img");
                img.src = movie.poster_path ? "https://image.tmdb.org/t/p/w500/" + movie.poster_path : movie.original_title;
                img.alt = movie.original_title;
-               /*
-               const divIcon = document.createElement("div");
-               divIcon.className = "container position-absolute top-5 end-5";
-               const heartIcon = document.createElement("i");
-               heartIcon.className = "fa-solid fa-heart";
-               const heartEmptyIcon = document.createElement("i");
-               heartEmptyIcon.className = "fa-solid fa-heart";
-               divIcon.appendChild(heartEmptyIcon);
-               */
                const divFavIcon = document.createElement("div");
                if (isInFavorites(movie.id)) {
                   divFavIcon.innerHTML = `<svg id="${movie.id}" xmlns="http://www.w3.org/2000/svg" stroke="white" fill="#ff000078" width="50" height="50"
@@ -79,20 +70,6 @@ function createMovies(method: Promise<Movie[]>): void {
                      console.log(changeFavIcon(movie.id, op));
                   });
                }
-               /*
-               const svg = document.createElement("svg");
-               svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-               svg.setAttribute("stroke", "red");
-               svg.setAttribute("fill", "#ff000078");
-               svg.setAttribute("width", "50");
-               svg.setAttribute("height", "50");
-               svg.className = "bi bi-heart-fill position-absolute p-2";
-               svg.setAttribute("viewBox", "0 -2 18 22");
-               const pathElem = document.createElement("path");
-               pathElem.setAttribute("fill-rule", "evenodd");
-               pathElem.setAttribute("d", "M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z");
-               svg.appendChild(pathElem);
-               */
                const divBody = document.createElement("div");
                divBody.className = "card-body";
                const p = document.createElement("p");
